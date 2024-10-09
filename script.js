bd967a693318a49d38311284060c3f658cce9c7b
Mohanad Abotaleb <mohanado2370@gmail.com>
2024-10-10 00:44:02 +0300

    Most basic functionality is done
    Adjusted CSS, added a blinking cursor, implemented keypress functionality, backspaces, and correctly displayed whether a letter is typed correctly or not.

--- script.js
+++ script.js
@@ -1,34 +1,95 @@
 const wordNum = 200;
+let wordIndex = 0;
 
 fetch('/1000-most-common-words.txt')
 .then((res) => res.text()) 
 .then((text) => {
     const words = text.split('\n');
     displayWords(words);
-    console.log(document.getElementsByClassName("words").innerHTML)
+
 })
 .catch((e) => console.log(e))
 
-function randWords(words) {
+function randWords(words, wordNum) {
     const index = Math.ceil(Math.random() * wordNum);
     return words[index];
 }
 
 function displayWords(words) {
     const element = document.getElementById("words");
-        element.innerHTML = '';
-        for(let i = 0; i < wordNum; i++) {
-            element.innerHTML += `<div class="word">
-            <span class="letter">
-            ${randWords(words).split('').join('</span><span class="letter">')}
-            </span>
-            </div>`;
-        }
+    element.innerHTML = '';
+    for(let i = 0; i < wordNum; i++) {
+        element.innerHTML += `<div class="word"><span class="letter">${randWords(words, wordNum).split('').join('</span><span class="letter">')}</span></div>`;
+    }
+    const firstWord = document.querySelector(".word");
+    const firstLetter = firstWord.querySelector(".letter");
+    setClass(firstWord, "current");
+    setClass(firstLetter, "current");
     }
 
 
-document.getElementById("game").addEventListener("keyup", (ev) => {
+function setClass(elName, clName) {
+    elName.className += ` ${clName}`;
+}
 
+function removeClass(elName, clName) {
+    elName.className = elName.className.replace(clName,'');
+}
+
+
+
+document.getElementById("game").addEventListener("keyup", (ev) => {
+    const key = ev.key;
+    const currentLetter = document.querySelector(".letter.current");
+    const currentWord = document.querySelector(".word.current");
+    let expected = currentLetter?.innerHTML || ' ';
+
+    if (key !== " " && key.length === 1) {
+        if (currentLetter) {
+            setClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+            removeClass(currentLetter, 'current');
+            if (currentLetter.nextSibling) {
+                setClass(currentLetter.nextSibling, 'current');
+            } 
+            
+        }
+    } else if (key === ' ') {
+            if (expected !== ' ') {
+            const incorrectLetters = [...document.querySelectorAll('.word.current .letter:not(.correct)')]
+            for (letter of incorrectLetters) {
+                setClass(letter, 'incorrect');
+            }
+
+        }
+        removeClass(currentWord, 'current');
+        setClass(currentWord.nextSibling, 'current');
+        if (currentLetter) {
+            removeClass(currentLetter, 'current');
+        }
+        setClass(currentWord.nextSibling.firstChild, 'current');
+    }
+    else if (key === 'Backspace') {
+        if (currentLetter && (currentLetter === currentWord.firstChild) && (currentWord !== document.querySelector('.word'))) {
+            removeClass(currentWord, "current");
+            setClass(currentWord.previousSibling, 'current');
+            removeClass(currentLetter, 'current');
+            setClass(currentWord.previousSibling.lastChild, 'current');
+            removeClass(currentWord.previousSibling.lastChild, 'correct');
+            removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+        }
+        else if (currentLetter && !(currentLetter === currentWord.firstChild)) {
+            removeClass(currentLetter, 'current');
+            setClass(currentLetter.previousSibling, 'current');
+            removeClass(currentLetter.previousSibling, 'correct');
+            removeClass(currentLetter.previousSibling, 'incorrect');
+
+        }
+        else if (!currentLetter) {
+            setClass(currentWord.lastChild, 'current');
+            removeClass(currentWord.lastChild, 'incorrect');
+            removeClass(currentWord.lastChild, 'correct');
+        }
+    }
 }
 );
 
